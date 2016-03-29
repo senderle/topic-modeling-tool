6bea69324b3024a193aec8def38d64098223c3bd
Scott Enderle <scott.enderle@gmail.com>
2016-03-29 12:44:29 -0400

    add testdata and one new emtpy testcase

---
+++ TopicModelingTool/src/test/java/cc/mallet/topics/gui/TopicModelOutputTest.java
@@ -0,0 +1,9 @@
+package cc.mallet.topics.gui;
+
+import junit.framework.TestCase;
+
+public class TopicModelOutputTest extends TestCase {
+    public void testTopicModelOutput() throws Exception {
+        assertEquals(true, true);
+    }
+}
